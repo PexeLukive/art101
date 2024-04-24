239332768ce5f9ab1496750e0a2e40a558601bcc
Rani Yeh <anlyeh@ucsc.edu>
2024-04-24 05:17:04 -0700

    lab 4 changes

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,18 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Rani Yeh
+// Date: 4/24/2024
+
+// An implementation of Tetris
+// Rani Yeh <anlyeh@ucsc.edu>
+// April 2024
+
+// Creating a Tetris game 
+// 1. Create empty board
+// 2. Select one out of six block shapes to be put on board, slowly descending to bottom of board 
+  // 3. Allow user to release block in desired position/rotation 
+  // 4. Test filled-out-line condition
+  // 5. Repeat from step 2 until board is filled to top 
+// 6. Display total score
 
 // Constants
 
