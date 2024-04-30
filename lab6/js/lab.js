a50b67be78c891fa919f0d0eeb48800efacd97fa
Rani Yeh <anlyeh@ucsc.edu>
2024-04-30 15:04:44 -0700

    lab 6 changes

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,8 +1,26 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Rani Yeh
+// Date: 4/30/2024
 
 // Constants
+
+const myTransport = ["loop bus", "metro", 'walk', "friend's car"];
+
+var myMainRide = {
+  make: "Toyota",
+  model: "Corolla",
+  color: "silver",
+  year: 2016,
+  age: function() {
+    return 2024 - this.year;
+  }
+}
+
+// Output
+
+document.writeln("Getting around: " + myTransport + "<br>");
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 // Functions
 
